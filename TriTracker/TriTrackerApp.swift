db395e55d6bde5f63909ae5271f6a384700fe7c8
Jacques Aucamp <jacquesaucamp99@gmail.com>
2023-04-01 16:56:56 +0200

    Initial Commit

---
+++ TriTracker/TriTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriTrackerApp.swift
+//  TriTracker
+//
+//  Created by Jacques Aucamp on 2023/04/01.
+//
+
+import SwiftUI
+
+@main
+struct TriTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
